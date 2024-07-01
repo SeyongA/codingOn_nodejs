558a0a504cf301c9b64d5d7a7d00861ab97c909b
SeyongA <tlstpdyd11@naver.com>
2024-07-01 20:51:57 +0900

    test

---
+++ 0628/index.js
@@ -0,0 +1,25 @@
+// function goMart() {
+//     console.log("마트에 가서 어떤 음료를 살지 고민한다.");
+// }
+
+// function pickDrink(callback) {
+// setTimeout(() => {
+//     console.log("고민끝");
+//     product = '제로콜라';
+//     price = 3000;
+//     callback(product, price);
+// }, 3000);
+// }
+
+// function payCallback(product, price) {
+// console.log(`상품명 : ${product}, 가격 : ${price}`);
+// }
+
+// let product;
+// let price;
+// goMart();
+// pickDrink(payCallback);
+
+// pickDrink(function (product, price) {
+// console.log(`상품명 ; ${product}, 가격: ${price}`);
+// })
